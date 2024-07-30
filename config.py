8bbfaf7af9885654a08897350a2ae92629be4c97
marcio <120094486+Marciohenr1que@users.noreply.github.com>
2024-07-30 07:52:43 -0300

    feat(docker): add Docker support for the application

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'  # URL do broker do Celery
+    CELERY_BROKER_URL = 'redis://localhost:6379/0'  #  gerenciamento de tarefas
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  # Backend de resultados
     PARCELVOY_API_KEY = 'coloca_uma_chave'
